22c1294c66882cd3a976ac0dc0313c56cdbad472
LAPPPROJECTS <64042366+LAPPPROJECTS@users.noreply.github.com>
2020-04-25 08:23:05 +0100

    Add files via upload

--- src/Car/CarReturn.java
+++ src/Car/CarReturn.java
@@ -1,5 +1,29 @@
 package Car;// klasa odpowiadajaca za zwracanie aut
 
 
-public class CarReturn {
+import java.util.Date;
+
+public class CarReturn extends CarRental {
+    private Date dateOfCustomerReturn;
+
+    public Date getDateOfCustomerReturn() {
+        return dateOfCustomerReturn;
+    }
+
+    public void setDateOfCustomerReturn(Date dateOfCustomerReturnCar) {
+        this.dateOfCustomerReturn = dateOfCustomerReturnCar;
+    }
+
+    public CarReturn(String vinNumber, String regNumber, String make, String model, String carColor,
+                     float pricePerDayRent, float pricePerMonthlyRent, float pricePerYearRent,
+                     String carAvailability) {
+        super(vinNumber, regNumber, make, model, carColor, pricePerDayRent, pricePerMonthlyRent,
+                pricePerYearRent, carAvailability);
+    }
+
+    public int hashMap(){
+        return this.getRegNumber().hashCode();
+    }
+
+
 }
